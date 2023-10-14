cf9bd6e4952af7b96d3286e331b0d71b867804ae
Fleet Release Manager <ay@mntw.re>
2023-10-14 16:08:36 +0200

    Disable formatter

--- server/src/main/kotlin/org/javacs/kt/formatting/Formatter.kt
+++ server/src/main/kotlin/org/javacs/kt/formatting/Formatter.kt
@@ -3,6 +3,8 @@ package org.javacs.kt.formatting
 import org.eclipse.lsp4j.FormattingOptions as LspFromattingOptions
 
 interface Formatter {
-    fun format(code: String, options: LspFromattingOptions): String
+    fun format(
+        code: String,
+        options: LspFromattingOptions,
+    ): String
 }
-
